96525670ebb88331fffa1a8cbb6bb87ea220af1e
kranthi <donadikranthikumar@gmail.com>
2025-09-28 20:40:11 +0530

    main

--- 23-while-loop.sh
+++ 23-while-loop.sh
@@ -4,7 +4,8 @@ count=5
 
 echo "Starting countdown..."
 while [ $count -gt 0 ]
-do echo "Time left: $count" sleep 1 # Pause for 1 second
+do echo "Time left: $count" 
+sleep 1  # Pause for 1 second
  count=$((count - 1)) # Decrement the count
 done
 
