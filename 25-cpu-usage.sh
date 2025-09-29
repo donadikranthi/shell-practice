1b44cb6d6c98865522e32e4a943dde8a91f9cf92
kranthi <donadikranthikumar@gmail.com>
2025-09-29 20:46:23 +0530

    main

--- 25-cpu-usage.sh
+++ 25-cpu-usage.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-DISK_USAGE=$(df -h / | grep -v Filesystem)
+DISK_USAGE=$(df -hT | grep -v Filesystem)
 DISK_THRESHOLD=2
 
 while IFS= read -r line
 
 do
-    USAGE=$(echo $line | awk '{print $5}' | cut -d "%" -f1)
-    PARTITION=$(echo $line | awk '{print $7}')
-    if [ $USAGE -ge $DISK_THRESHOLD ]; then
-        echo "High disk usage on $PARTITION: $USAGE"
-    fi
+    echo "ine:$line"
+    
 done <<< $DISK_USAGE
