8774c3322e96cb9a78f8593827bfd79862b2fde9
kranthi <donadikranthikumar@gmail.com>
2025-09-30 13:33:35 +0530

    main

--- Linux.sh
+++ Linux.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Declare an associative array
-declare -A user_data
 
 # Populate user data
 user_data["john"]="John Doe,john@example.com,Admin"
@@ -10,8 +9,4 @@ user_data["mike"]="Mike Johnson,mike@example.com,Editor"
 user_data["sara"]="Sara Davis,sara@example.com,User"
 user_data["david"]="David Lee,david@example.com,Guest"
 
-echo "User details:"
-for username in "${!user_data[@]}"; do
-    IFS=',' read -r fullname email role <<< "${user_data[$username]}"
-    echo "Username: $username, Full Name: $fullname, Email: $email, Role: $role"
-done
+
